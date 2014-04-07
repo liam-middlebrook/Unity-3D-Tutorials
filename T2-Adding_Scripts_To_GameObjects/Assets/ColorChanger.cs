eee4f44ec571b2990f1c95902de379e2d9526806
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-04-07 16:41:26 -0400

    Updated Tutorial 2

---
+++ T2-Adding_Scripts_To_GameObjects/Assets/ColorChanger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorChanger : MonoBehaviour
+{
+	//Creates a variable that can be changed in the Unity Editor
+	public Color tintColor;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//Changes the color of the material we are going to render
+		this.renderer.material.color = tintColor;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
