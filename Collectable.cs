ddd7c781c7606f03e606d4702673baaa6b299b53
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-07-09 00:06:37 -0400

    Added collectable.cs

---
+++ Collectable.cs
@@ -0,0 +1,45 @@
+﻿/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Liam Middlebrook ( https://github.com/liam-middlebrook )
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using UnityEngine;
+using System.Collections;
+
+/*
+ * A Collectable class for unity3d
+ * 
+ * On collision the collectable destroys itself
+*/
+public class Collectable : MonoBehaviour {
+
+    // When the collectable is collided with by any game object
+    void OnCollisionEnter(Collision col)
+    {
+        // If we collide with the player do the following
+        if (col.gameObject.tag == "Player")
+        {
+            // Destroy this collectable
+            Destroy(this.gameObject);
+        }
+    }
+}
